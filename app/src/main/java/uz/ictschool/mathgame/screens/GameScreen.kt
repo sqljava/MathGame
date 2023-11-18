9ab21f5acf3810b24948800af8588dd0bf0ac2f2
ShopOnline <komashuxratov@gmail.com>
2023-11-18 09:55:15 +0500

    w

--- app/src/main/java/uz/ictschool/mathgame/screens/GameScreen.kt
+++ app/src/main/java/uz/ictschool/mathgame/screens/GameScreen.kt
@@ -1,10 +1,15 @@
 package uz.ictschool.mathgame.screens
 
 import android.util.Log
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -21,6 +26,44 @@ import androidx.compose.ui.unit.sp
 private var randomNumber = 0
 
 
+
+
+
+
+
+fun random():String{
+    randomNumber = (1 until 9).random()
+
+    var num1 = (1 until 9).random()
+    var num2 = (1 until 9).random()
+    var signs = listOf("+","-","*","/")
+    var answer = signs.random()
+
+    if (answer == "/"){
+        while (num1 % num2 != 0){
+            num1 = (1 until 9).random()
+            num2 = (1 until 9).random()
+        }
+    }
+
+     var question = "$num1 _ $num2 = ${calculate(num1, num2, answer)}"
+    return question
+}
+
+fun calculate(num1 : Int, num2: Int, sign: String): Int{
+    return when(sign){
+        "+"->num1 + num2
+        "-"->num1-num2
+        "*"->num1 * num2
+        "/"->num1 / num2
+        else -> {
+            5
+        }
+    }
+}
+
+
+
 @Preview(showBackground = true)
 @Composable
 fun GameScreen(){
@@ -29,24 +72,66 @@ fun GameScreen(){
         mutableStateOf(0)
     }
 
+    var question by remember {
+        mutableStateOf(random())
+    }
+
+    random()
+
     Box(modifier = Modifier
-        .fillMaxSize()
-        .padding(vertical = 100.dp),
-        contentAlignment = Alignment.TopCenter,
+        .fillMaxSize(),
+        contentAlignment = Alignment.Center,
 
     ){
 
-        Column {
-            Text(text = "2 [ $num ] 3",
+        Column(modifier = Modifier.fillMaxWidth(),
+            horizontalAlignment = Alignment.CenterHorizontally) {
+            Text(text = question,
                 fontSize = 20.sp)
 
-            Button(onClick = {
-                num = (1 until 9).random()
-                randomNumber = num
-                Log.d("TAG", "GameScreen: $randomNumber")
-            },
-                modifier = Modifier.padding(10.dp)) {
-                Text(text = "random")
+//            Button(onClick = {
+//                num = (1 until 9).random()
+//                randomNumber = num
+//                Log.d("TAG", "GameScreen: $randomNumber")
+//            },
+//                modifier = Modifier.padding(10.dp)) {
+//                Text(text = "random")
+//            }
+
+
+
+            Row(modifier = Modifier
+                .fillMaxWidth()
+                .padding(vertical = 10.dp),
+                horizontalArrangement = Arrangement.Center) {
+                Button(onClick = {
+                    random()
+                    Log.d("TAG", "GameScreen: $randomNumber")
+                }) {
+                    Text(text = "+")
+
+                }
+                Spacer(modifier = Modifier.width(30.dp))
+
+                Button(onClick = { /*TODO*/ }) {
+                    Text(text = "-")
+
+                }
+
+            }
+
+            Row {
+                Button(onClick = { /*TODO*/ }) {
+                    Text(text = "*")
+
+                }
+                Spacer(modifier = Modifier.width(30.dp))
+
+                Button(onClick = { /*TODO*/ }) {
+                    Text(text = "/")
+
+                }
+
             }
         }
 
